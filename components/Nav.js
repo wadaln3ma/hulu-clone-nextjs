48ec4b233650daa4eb9cc22b4e5886bf9bd25a57
Abdulrahman Hamad <wadaln3ma@gmail.com>
2022-01-24 13:53:46 +0200

    Create Navbar

---
+++ components/Nav.js
@@ -0,0 +1,23 @@
+import {useRouter} from 'next/router'
+import requests from '../utils/requests'
+
+const Nav = ()=>{
+  const router = useRouter()
+
+  return(
+    <nav className="relative">
+      <div className="flex space-x-10 sm:space-x-30 whitespace-nowrap px-10 sm:px-20 overflow-scroll no-scrollbar">
+        {
+          Object.entries(requests).map(([key, {title, url}]) =>(
+            <h2 key={key}
+              onClick={()=> router.push(`/?genre=${key}`)}
+            className="last:pr-20 text-2xl hover:text-white transition transition-durafion-100 tranform hover:scale-125 active:text-red-500 cursor-pointer">{title}</h2>
+          ))
+        }
+      </div>
+      <div className="absolute top-0 right-0 h-10 w-1/12 bg-gradient-to-l from-[#06202A]"/>
+      <div className="absolute top-0 left-0 h-10 w-1/12 bg-gradient-to-r from-[#06202A]"/>
+    </nav>
+)}
+
+export default Nav
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 
 export default function Home() {
   return (
@@ -13,6 +13,7 @@ export default function Home() {
 
       <Header />
 
+      <Nav />
   
     </div>
   )
