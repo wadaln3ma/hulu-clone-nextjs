00f44cc4e15cce4eebece775543eaa5ec85fda6a
Abdulrahman Hamad <wadaln3ma@gmail.com>
2022-01-25 15:13:23 +0200

    fixing a typo

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import FlipMove from "react-flip-move"
 
 const Results = ({ movies })=>{
   return (
-    <FlipMove className="px-5 my-10 sm:grid md:grid-col-2 xl:grid-col-3 3xl:flex flex-wrap justify-center">
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {movies.results.map( movie =>(
         <MovieItem key={movie.id}
           movie={movie}/>
